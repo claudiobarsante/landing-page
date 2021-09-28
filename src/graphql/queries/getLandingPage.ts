6e9bcc0b3926b3af72ffe2c44f201f577fe7d979
Claudio Barsante <clbmribas@gmail.com>
2021-09-28 14:46:24 -0300

    feat: add header to GET_LANDING_PAGE query

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,30 @@
-const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
+import { gql } from 'graphql-request'
+const GET_LANDING_PAGE = gql`
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
         alternativeText
         url
       }
+    }
+  }
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
     }
   }
 `
